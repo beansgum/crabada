c2cad359e80d4fd3d685a64a7db097a1517434c2
Olanrewaju Collins <ocollins444@gmail.com>
2021-11-24 05:01:50 +0100

    Delete js code

--- utils.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    sleep: function (seconds) {
-        var e = new Date().getTime() + (seconds * 1000);
-        while (new Date().getTime() <= e) { }
-    },
-    currentTimeSeconds: function () {
-        return new Date().getTime() / 1000
-    }
-}
